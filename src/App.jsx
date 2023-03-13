dc538eaf300f5f7ce17aef43fe7a5efcddcc4d9d
sz44 <semion44@gmail.com>
2023-03-12 21:00:15 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Container } from "./components/styles/Container.styled";
+import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors : {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
+
+function App() {
+  return (
+    <ThemeProvider theme = {theme}>
+      <>
+        <Header />
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
